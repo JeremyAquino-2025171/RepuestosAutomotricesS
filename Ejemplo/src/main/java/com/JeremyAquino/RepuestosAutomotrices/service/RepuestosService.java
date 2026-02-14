7d6dc03c28e08e4332807f8f32904035dea7589e
JeremyAquino-2025171 <jaquino-2025171@kinal.edu.gt>
2026-02-14 08:55:40 -0600

    feat: se agrego la interface Repuestos service ya terminada

--- Ejemplo/src/main/java/com/JeremyAquino/RepuestosAutomotrices/service/RepuestosService.java
+++ Ejemplo/src/main/java/com/JeremyAquino/RepuestosAutomotrices/service/RepuestosService.java
@@ -1,4 +1,12 @@
 package com.JeremyAquino.RepuestosAutomotrices.service;
 
+import com.JeremyAquino.RepuestosAutomotrices.entity.Repuestos;
+import java.util.List;
+
 public interface RepuestosService {
+    List<Repuestos> getAllRepuestos();
+    Repuestos getRepuestosById(Integer id);
+    Repuestos saveRepuestos(Repuestos repuestos);
+    Repuestos updateRepuestos(Integer id, Repuestos repuestos);
+    void deleteRepuestos(Integer id);
 }
