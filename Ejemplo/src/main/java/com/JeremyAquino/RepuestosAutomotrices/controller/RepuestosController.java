e6e2af7ba8478f2a0a0811e6311391cb93d0904a
JeremyAquino-2025171 <jaquino-2025171@kinal.edu.gt>
2026-02-14 08:52:38 -0600

    feat: se agrego la clase repuestos controller

--- Ejemplo/src/main/java/com/JeremyAquino/RepuestosAutomotrices/controller/RepuestosController.java
+++ Ejemplo/src/main/java/com/JeremyAquino/RepuestosAutomotrices/controller/RepuestosController.java
@@ -1,4 +1,46 @@
 package com.JeremyAquino.RepuestosAutomotrices.controller;
 
+import com.JeremyAquino.RepuestosAutomotrices.entity.Repuestos;
+import com.JeremyAquino.RepuestosAutomotrices.service.RepuestosService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Repuestos")
 public class RepuestosController {
+
+    private final RepuestosService repuestosService;
+
+    public RepuestosController(RepuestosService repuestosService) {
+        this.repuestosService = repuestosService;
+    }
+
+    @GetMapping
+    public List<Repuestos> getAllRepuestos() {
+        return repuestosService.getAllRepuestos();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createRepuestos(@Valid @RequestBody Repuestos repuestos) {
+        try {
+            Repuestos createdRepuestos = repuestosService.saveRepuestos(repuestos);
+            return new ResponseEntity<>(createdRepuestos, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteRepuestos(@PathVariable Integer id) {
+        try {
+            repuestosService.deleteRepuestos(id);
+            return ResponseEntity.ok("El repuesto fue eliminado con éxito: " + id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo eliminar: " + e.getMessage());
+        }
+    }
 }
