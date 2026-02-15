74f8c28aee570893d28bdff5d5cbc0211d236b2d
JeremyAquino-2025171 <jaquino-2025171@kinal.edu.gt>
2026-02-15 16:38:26 -0600

    feat: se agrego la clase VentasController

--- Ejemplo/src/main/java/com/JeremyAquino/RepuestosAutomotrices/controller/VentasController.java
+++ Ejemplo/src/main/java/com/JeremyAquino/RepuestosAutomotrices/controller/VentasController.java
@@ -1,4 +1,47 @@
 package com.JeremyAquino.RepuestosAutomotrices.controller;
 
+import com.JeremyAquino.RepuestosAutomotrices.entity.Ventas;
+import com.JeremyAquino.RepuestosAutomotrices.service.VentasService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Ventas")
 public class VentasController {
+
+    private final VentasService ventasService;
+
+
+    public VentasController(VentasService ventasService) {
+        this.ventasService = ventasService;
+    }
+
+    @GetMapping
+    public List<Ventas> getAllVentas() {
+        return ventasService.getAllVentas();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createVentas(@Valid @RequestBody Ventas ventas) {
+        try {
+            Ventas createdVentas = ventasService.saveVentas(ventas);
+            return new ResponseEntity<>(createdVentas, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteVentas(@PathVariable Integer id) {
+        try {
+            ventasService.deleteVentas(id);
+            return ResponseEntity.ok("La venta fue eliminada con éxito: " + id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo eliminar: " + e.getMessage());
+        }
+    }
 }
